95a993a4cd3b0accee6f95374fde4cd30c1c8194
Joel Parker Henderson <joel@joelparkerhenderson.com>
2016-07-04 12:39:48 -0400

    Rename demo workbook

--- Demo Swift Xlsx Reader Writer/Demo Swift Xlsx Reader Writer/ViewController.swift
+++ Demo Swift Xlsx Reader Writer/Demo Swift Xlsx Reader Writer/ViewController.swift
@@ -4,7 +4,7 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    let path: String = NSBundle.mainBundle().pathForResource("DemoWorkbook", ofType: "xlsx")!
+    let path: String = NSBundle.mainBundle().pathForResource("demo", ofType: "xlsx")!
     let spreadsheet: BRAOfficeDocumentPackage = BRAOfficeDocumentPackage.open(path)
     let worksheet: BRAWorksheet = spreadsheet.workbook.worksheets[0] as! BRAWorksheet
     let string: String = worksheet.cellForCellReference("A1").stringValue()
