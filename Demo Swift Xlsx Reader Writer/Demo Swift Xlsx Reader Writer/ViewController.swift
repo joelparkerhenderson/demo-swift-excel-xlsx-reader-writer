4e71aefdf4eaa14f8ed22b01272a3df56b2428a1
Joel Parker Henderson <joel@joelparkerhenderson.com>
2017-09-17 19:04:15 +0700

    Update for Swift 3

--- Demo Swift Xlsx Reader Writer/Demo Swift Xlsx Reader Writer/ViewController.swift
+++ Demo Swift Xlsx Reader Writer/Demo Swift Xlsx Reader Writer/ViewController.swift
@@ -4,11 +4,11 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    let path: String = NSBundle.mainBundle().pathForResource("demo", ofType: "xlsx")!
+    let path: String = Bundle.main.path(forResource: "demo", ofType: "xlsx")!
     let spreadsheet: BRAOfficeDocumentPackage = BRAOfficeDocumentPackage.open(path)
     let sheet: BRASheet = spreadsheet.workbook.sheets[0] as! BRASheet
     let worksheet: BRAWorksheet = spreadsheet.workbook.worksheets[0] as! BRAWorksheet
-    let cell: BRACell = worksheet.cellForCellReference("A1")
+    let cell: BRACell = worksheet.cell(forCellReference: "A1")
     print(sheet.name) // print "Sheet1"
     print(cell.stringValue()) // print "Alpha"
   }
